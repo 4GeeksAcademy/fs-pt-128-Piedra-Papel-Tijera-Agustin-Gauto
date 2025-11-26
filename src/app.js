4d7031575c94ce32c3c54e9cf42a485ad7b5e619
AgustinMGauto <agustin_gauto@outlook.com>
2025-11-26 19:01:06 +0000

    refactorizacion del proyecto y agrego comentarios

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
+// opciones a elegir del jugador
 const choises = ["rock", "paper", "scissors", "lizard", "spock"]
+
+// reglas del juego
 const rules = {
     rock: ['scissors', "lizard"],
     paper: ["rock", "spock"],
@@ -8,33 +11,29 @@ const rules = {
 }
 
 
-
+/// funcion que eligue aleatoriamente una opcion
 function eleccionPC(arr) {
     const num = Math.floor(Math.random() * arr.length)
     return arr[num]
 };
 
+
 const GanaUser = (Opciones, elecciondecomputadora) => {
     for (const opcion of Opciones) {
-        if (opcion === elecciondecomputadora) {
-            return true
-        }
+        if (opcion === elecciondecomputadora) return true
         return false
     }
 }
 
+/// funcion que utiliza funciones anteriores para iniciar el juego
 function jugar(userChoice) {
     const PCRAND = eleccionPC(choises)
-    console.log("El usuario Eligio:", userChoice);
-    console.log("Computer Eligio:", PCRAND);
-
+    console.log("El usuario Eligio:", userChoice); console.log("Computer Eligio:", PCRAND);
+    
 
     if (userChoice === PCRAND) { return "Empate" }
-    else if (GanaUser(rules[userChoice], PCRAND)){
-        return "Gana Usuario"
-    }else {
+    if (GanaUser(rules[userChoice], PCRAND)) return "Gana Usuario"
         return "Gana Computer"
-    }
 }
 
 
